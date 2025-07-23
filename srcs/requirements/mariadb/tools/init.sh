6758a0c2324490df7dc81a2ed435635500ab9aea
agerard <antoine.gerard@intuitim.com>
2025-07-23 18:22:56 +0200

    mods mariadb dockerfile

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,47 +1,28 @@
 #!/bin/bash
 
-# Démarrer le service MariaDB
-service mysql start
+set -e
 
-# Lire les variables depuis les fichiers secrets
-root_pwd_file="/secrets/db_root_password.txt"
-user_pwd_file="/secrets/db_password.txt"
+# Lire les variables
+ROOT_PWD=$(cat /secrets/db_root_password.txt)
+USER_PWD=$(cat /secrets/db_password.txt)
+DB_NAME=$(cat /secrets/db_name.txt)
+DB_USER=$(cat /secrets/db_user.txt)
 
-db_name_file="/secrets/db_name.txt"
-db_user_file="/secrets/db_user.txt"
+# Démarrer MariaDB temporairement
+mysqld_safe --skip-networking &
+sleep 5
 
-if [ -f "$root_pwd_file" ]
-then
-    ROOT_PWD=$(cat "$root_pwd_file")
-fi
+# Créer base et utilisateur
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$USER_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD';
+FLUSH PRIVILEGES;
+EOF
 
-if [ -f "$user_pwd_file" ]
-then
-    USER_PWD=$(cat "$user_pwd_file")
-fi
+# Arrêter MariaDB temporaire
+mysqladmin -u root -p"$ROOT_PWD" shutdown
 
-if [ -f "$db_name_file" ]
-then
-    DB_NAME=$(cat "$db_name_file")
-fi
-
-if [ -f "$db_user_file" ]
-then
-    DB_USER=$(cat "$db_user_file")
-fi
-
-# Créer les commandes SQL dynamiquement
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$USER_PWD';" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD';" >> db.sql
-echo "FLUSH PRIVILEGES;" >> db.sql
-
-# Exécuter les commandes SQL
-mysql < db.sql
-
-# Nettoyer le processus initial et lancer mariadb
-kill $(cat /var/run/mysqld/mysqld.pid)
-
+# Lancer MariaDB (PID 1)
 exec mysqld
-
